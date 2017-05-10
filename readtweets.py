de3c9ba5ecd6e2a0f80d74c89d6358fdb9682388
saphraxaeris <stephan.elias@upr.edu>
2017-05-09 22:12:12 -0400

    pls work ahora

--- readtweets.py
+++ readtweets.py
@@ -30,38 +30,38 @@ def read_tweets(access_token, access_secret, consumer_key, consumer_secret):
     print("Creating producer...")
     producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-    # print("Connecting to twitter...")
+    print("Connecting to twitter...")
     # Initiate the connection to Twitter Streaming API
-    # twitter_stream = TwitterStream(auth=oauth)
+    twitter_stream = TwitterStream(auth=oauth)
 
-    # # Get a sample of the public data following through Twitter
-    # iterator = twitter_stream.statuses.sample()
+    # Get a sample of the public data following through Twitter
+    iterator = twitter_stream.statuses.sample()
 
-    # # Print each tweet in the stream to the screen
-    # # Here we set it to stop after getting 1000 tweets.
-    # # You don't have to set it to stop, but can continue running
-    # # the Twitter API to collect data for days or even longer.
-    # tweet_count = 10
-    # print("Stating to read tweets")
-    # for tweet in iterator:
-    #     tweet_count -= 1
-    #     # Twitter Python Tool wraps the data returned by Twitter
-    #     # as a TwitterDictResponse object.
-    #     try:
-    #         producer.send('tweets', str(tweet))
+    # Print each tweet in the stream to the screen
+    # Here we set it to stop after getting 1000 tweets.
+    # You don't have to set it to stop, but can continue running
+    # the Twitter API to collect data for days or even longer.
+    tweet_count = 10
+    print("Stating to read tweets")
+    for tweet in iterator:
+        tweet_count -= 1
+        # Twitter Python Tool wraps the data returned by Twitter
+        # as a TwitterDictResponse object.
+        try:
+            producer.send('tweets', str(tweet))
 
-    #         # print screen_name and name
-    #         # print("TWEET: ", tweet['user']['screen_name'])
-    #         # The command below will do pretty printing for JSON data, try it out
-    #         # print("TWEET JSON: ", json.dumps(tweet, indent=4))
-    #         # This next command, prints the tweet as a string
-    #         # print ("TWEETS STRING", str(tweet))
-    #     except:
-    #         pass
+            # print screen_name and name
+            # print("TWEET: ", tweet['user']['screen_name'])
+            # The command below will do pretty printing for JSON data, try it out
+            # print("TWEET JSON: ", json.dumps(tweet, indent=4))
+            # This next command, prints the tweet as a string
+            # print ("TWEETS STRING", str(tweet))
+        except:
+            pass
 
-    #     if tweet_count <= 0:
-    #         print("Done")
-    #         break
+        if tweet_count <= 0:
+            print("Done")
+            break
 
 if __name__ == "__main__":
     credentials = read_credentials()
