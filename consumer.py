187a13da228b6595823368ea06b9ba23dc73065b
saphraxaeris <stephan.elias@upr.edu>
2017-05-17 23:55:35 -0400

    didnt add a return...

--- consumer.py
+++ consumer.py
@@ -24,7 +24,7 @@ def VerifyHashtag(word):
 
 def VerifyNotUnicode(word):
     if isinstance(word, str):
-        return
+        return word
 
 def VerifyNotStopWord(word):
     if len(word) > 3 and word not in ['a','about','above','after','again','against','all','am','an','and','any','are','as','at','be','because','been','before','being','below','between','both','but','by','cannot','could','did','do','does','doing','down','during','each','few','for','from','further','had','has','have','having','he','her','here','hers','herself','him','himself','his','how','i','if','in','into','is','it','its','itself','me','more','most','my','myself','no','nor','not','of','off','on','once','only','or','other','ought','our','ours','ourselves','out','over','own','same','she','should','so','some','such','than','that','the','their','theirs','them','themselves','then','there','these','they','this','those','through','to','too','under','until','up','very','was','we','were','what','when','where','which','while','who','whom','why','with','would','you','your','yours','yourself','yourselves']:
